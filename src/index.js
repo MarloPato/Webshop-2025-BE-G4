8424255887e0f3d2fe3debfbd645f577c19c9cf0
Gabriel Richter Reynoso <garey91@gmail.com>
2025-03-18 11:47:30 +0100

    Connect to database

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/hakim-livs')
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB', process.env.MONGODB_URI))
   .catch(err => console.error('MongoDB connection error:', err));
 
