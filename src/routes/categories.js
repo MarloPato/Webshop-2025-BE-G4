aeee69ad2f77a70a2613ee3ea731c93397a9e955
Gabriel Richter Reynoso <garey91@gmail.com>
2025-03-26 10:55:32 +0100

    Get categories added

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -3,6 +3,16 @@ import Category from "../models/Category.js";
 
 const router = express.Router();
 
+router.get('/', async(req,res) => {
+  try {
+      const categories = await Category.find();
+      res.json(categories);
+      return;
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+})
+
 router.post("/", async (req, res) => {
   try {
     const newCategory = await new Category(req.body);
