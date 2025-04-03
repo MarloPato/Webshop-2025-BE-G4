1df64b40a476005ea26c84d926d8b3e1d91f20ac
Gabriel Richter Reynoso <garey91@gmail.com>
2025-04-03 23:17:25 +0200

    fix category only contain letters

--- src/models/Category.js
+++ src/models/Category.js
@@ -4,9 +4,12 @@ const categorySchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
-        unique:true,
-        lowercase: true
+        unique: true,
+        lowercase: true,
+        trim: true,
+        match: [/^[a-zA-Z]+$/, 'Category name can only contain letters']
     }
-})
+}, { timestamps: true });
+
 
 export default mongoose.model('Category',categorySchema);
