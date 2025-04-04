f04adeb2dc300aad12f206270df78e580d08a82d
Gabriel Richter Reynoso <garey91@gmail.com>
2025-04-04 11:38:26 +0200

    activate category in product schema, not required.

--- src/models/Product.js
+++ src/models/Product.js
@@ -21,11 +21,10 @@ const productSchema = new mongoose.Schema({
     default: 0,
     min: 0
   },
-  // category: {
-  //   type: mongoose.Schema.Types.ObjectId,
-  //   ref: 'Category',
-  //   required: true
-  // },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+  },
   imageUrl: {
     type: String,
     required: true
